45e53b373758796f3112e2ce183d6f4c2e0e18a1
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-08 11:18:05 +0530

    Pop And Peek

--- src/com/bridgelabz/stack/DataStrcture.java
+++ src/com/bridgelabz/stack/DataStrcture.java
@@ -9,6 +9,11 @@ public class DataStrcture {
 		stack.push(56);
 		stack.push(30);
 		stack.push(70);
-		System.out.println(stack);
-	}
+		System.out.println("Before pop"+stack);
+		stack.pop();
+		System.out.println("-----------------------");
+		System.out.println("After Pop"+stack);
+		System.out.println("-------------------------");
+	    System.out.println(	"Top Elements "+stack.peek());
+;	}
 }
