f75ee55a5484b7559c3f71f21b7c1a2d9000de24
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-04-08 11:10:21 +0530

    Push Elemnt

---
+++ src/com/bridgelabz/stack/DataStrcture.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.stack;
+
+import java.util.Stack;
+
+public class DataStrcture {
+     
+	public static void main(String args[]) {
+		Stack stack=new Stack();
+		stack.push(56);
+		stack.push(30);
+		stack.push(70);
+		System.out.println(stack);
+	}
+}
